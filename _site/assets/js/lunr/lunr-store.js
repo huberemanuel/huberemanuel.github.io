1d52e41d46bcf455a7c719b018b035b608420fa3
Emanuel Huber <emanuel.tesv@gmail.com>
2021-05-14 00:32:39 -0300

    Removing _site

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Using UDPipe 2",
-        "excerpt":" ","categories": ["parsing"],
-        "tags": [],
-        "url": "/parsing/udpipe2/",
-        "teaser": null
-      }]
