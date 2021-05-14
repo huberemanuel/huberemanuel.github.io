a4b4fd3e512dd655879e60225543988b47959097
Emanuel Huber <emanuel.tesv@gmail.com>
2021-05-14 00:31:20 -0300

    Added _site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Using UDPipe 2",
+        "excerpt":" ","categories": ["parsing"],
+        "tags": [],
+        "url": "/parsing/udpipe2/",
+        "teaser": null
+      }]
